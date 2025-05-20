8c6f359ab39bd49acde10c7a77dda718161ffa85
SHIVANJANTHECREATOROFUNIVERSE <84670145+SHIVANJANTHECREATOROFUNIVERSE@users.noreply.github.com>
2025-05-21 01:28:20 +0530

    Update answer.py

--- codelingo/pages/answer.py
+++ codelingo/pages/answer.py
@@ -13,9 +13,11 @@ def wrong():
     st.write("Try again")
 
 
-def strans(b):
-    a = st.text_input("Your answer:")
-    if a == b:
-        st.success("Correct!")
-    elif a != b :
-        st.write("wrong answer")
+def strans(correct_answers):
+    answer = st.text_input("Your answer:")
+    if not answer.strip():
+        st.warning("Please enter something")
+    elif answer.strip().lower() in [x.lower() for x in correct_answers]:
+        st.success("✅ Correct!")
+    else:
+        st.error("❌ Wrong answer")
